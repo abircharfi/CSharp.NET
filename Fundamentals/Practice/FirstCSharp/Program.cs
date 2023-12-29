d7af0e16155eb842019abd570a2f2ac47b3607dd
Abir Charfi <abir8charfi@gmail.com>
2023-12-29 19:01:20 +0100

    update

--- Fundamentals/Practice/FirstCSharp/Program.cs
+++ Fundamentals/Practice/FirstCSharp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string[] myArray = { "apple", "banana", "cherry" };
+Console.WriteLine(string.Join(", ", myArray));
---
+++ Fundamentals/Practice/FundamentalsI/Program.cs
@@ -0,0 +1,44 @@
+﻿//Loop that prints all values from 1-255
+
+for (int i = 1; i < 256; i++)
+{
+Console.WriteLine(i);
+}
+// loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+for (int i = 1; i <= 100 ; i++)
+{
+ if (i % 3 == 0 || i % 5 == 0){
+    Console.WriteLine(i);
+ }
+}
+//loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+
+for (int i = 1; i <= 100 ; i++)
+{
+if (i % 3 == 0 ){
+    Console.WriteLine("Fizz");
+ }
+ if (i % 5 == 0 ){
+    Console.WriteLine("Buzz");
+ }
+ if (i % 3 == 0 && i % 5 == 0){
+    Console.WriteLine("FizzBuzz");
+ }
+}
+
+// while to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+
+int i = 1 ;
+while (i <= 100)
+{
+  if (i % 3 == 0 ){
+    Console.WriteLine("Fizz");
+ }
+ if (i % 5 == 0 ){
+    Console.WriteLine("Buzz");
+ }
+ if (i % 3 == 0 && i % 5 == 0){
+    Console.WriteLine("FizzBuzz");
+ }
+ i++ ;  
+} 
