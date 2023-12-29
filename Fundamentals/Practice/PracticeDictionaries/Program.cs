28fb3f52e7bb67403b4120b57bc058e0553c83e5
Abir Charfi <abir8charfi@gmail.com>
2023-12-29 20:08:26 +0100

    update

---
+++ Fundamentals/Practice/PracticeDictionaries/Program.cs
@@ -0,0 +1,16 @@
+﻿Dictionary<string,string> profile = new Dictionary<string,string>();
+//Almost all the methods that exists with Lists are the same with Dictionaries
+profile.Add("Name", "Speros");
+profile.Add("Language", "PHP");
+profile.Add("Location", "Greece");
+Console.WriteLine("Instructor Profile");
+Console.WriteLine("Name - " + profile["Name"]);
+Console.WriteLine("From - " + profile["Location"]);
+Console.WriteLine("Favorite Language - " + profile["Language"]);
+
+foreach (var entry in profile)
+{
+    Console.WriteLine(entry.Key + " - " + entry.Value);
+}
+
+
---
+++ Fundamentals/Practice/PracticeTest/Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> bikes = new List<string>();
+//Use the Add function in a similar fashion to push
+bikes.Add("Kawasaki");
+bikes.Add("Triumph");
+bikes.Add("BMW");
+bikes.Add("Moto Guzzi");
+bikes.Add("Harley Davidson");
+bikes.Add("Suzuki");
+//Accessing a generic list value is the same as you would an array
+Console.WriteLine(bikes[2]); //Prints "BMW"
+Console.WriteLine("The current manufacturers we have seen are:");
+for (var idx = 0; idx < bikes.Count; idx++)
+{
+    Console.WriteLine("-" + bikes[idx]);
+}
+// Insert a new item between a specific index
+bikes.Insert(2, "Yamaha");
+//Removal from Generic List
+//Remove is a lot like Javascript array pop, but searches for a specified value
+//In this case we are removing all manufacturers located in Japan
+bikes.Remove("Suzuki");
+//bikes.Remove("Yamaha");
+bikes.RemoveAt(0); //RemoveAt has no return value however
+//The updated list can then be iterated through using a foreach loop
+Console.WriteLine("List of Non-Japanese Manufacturers:");
+foreach (string manu in bikes)
+{
+    Console.WriteLine("-" + manu);
+}
