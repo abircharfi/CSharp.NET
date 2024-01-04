76d02e81384a89522c9a49fc2cfb5f462cec6345
Abir Charfi <abir8charfi@gmail.com>
2024-01-04 18:14:35 +0100

    update

---
+++ Fundamentals/Practice/Boxing/Unboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿
+List<object> myList = new List<object>();
+
+myList.Add(7);
+myList.Add(28);
+myList.Add(-1);
+myList.Add(true);
+myList.Add("chair");
+
+foreach (var item in myList)
+{
+   Console.WriteLine(item);
+}
+
+int sum = 0;
+
+        foreach (var item in myList)
+        {
+            if (item is int)
+            {
+                sum += (int)item;
+            }
+        }
+
+Console.WriteLine(sum);
+
+
