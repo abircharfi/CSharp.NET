c821ef656eb75b98856188f9b31dfd585b132f33
Abir Charfi <abir8charfi@gmail.com>
2024-01-24 11:38:48 +0100

    update

--- ASP.NETCore/Practice/DateValidator/Validations/FutureDateAttribute.cs
+++ ASP.NETCore/Practice/DateValidator/Validations/FutureDateAttribute.cs
@@ -7,9 +7,7 @@ public class FutureDateAttribute : ValidationAttribute
 {
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        // You first may want to unbox "value" here and cast to to a DateTime variable!
-
-         
+    
         if (value is DateTime date)
         {
             if (date > DateTime.Now)
